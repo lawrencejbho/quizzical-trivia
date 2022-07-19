213b7841137a80317208176fe2b78ea2c0212a63
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-07-19 14:20:51 -0700

    added in axios, displaying api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import sanitizeHtml from "sanitize-html"; // need this to help with the api format
 import Starter from "./components/Starter.js";
 
+// API - https://opentdb.com/api_config.php
+// https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
+
 function App() {
+  // for the starter screen
   const [start, setStart] = useState(false);
 
   function isStart() {
@@ -10,9 +16,36 @@ function App() {
 
   const starterElements = !start && <Starter isStart={isStart} />;
 
+  // for the quiz API
+
+  const [quiz, setQuiz] = useState([]);
+
+  const getQuiz = () => {
+    Axios.get("https://opentdb.com/api.php?amount=5").then((response) => {
+      const data = response.data.results;
+      // console.log(data);
+      setQuiz(data);
+    });
+  };
+
   return (
     <div className="App">
       {starterElements}
+      <button onClick={getQuiz}>Get Quiz</button>
+      {/* map through the array, destructure the elements and render as necessary */}
+      {quiz.map(
+        ({
+          category: category,
+          question: question,
+          correct_answer: correct_answer,
+          incorrect_answers: incorrect_answers,
+        }) => (
+          <div>
+            <div>{sanitizeHtml(category)}</div>
+            <div>{sanitizeHtml(question)}</div>
+          </div>
+        )
+      )}
       <h1> test code</h1>
     </div>
   );
