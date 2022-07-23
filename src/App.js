4a79e3eb956c9fb2bc13e784ce1f2280fdbeca61
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-07-22 21:47:32 -0700

    working on figuring out how to get the onClick to conditionally render

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Axios from "axios";
 import sanitizeHtml from "sanitize-html"; // need this to help with the api format
 import Starter from "./components/Starter.js"; // testing asdf
 import Answer from "./components/Answer.js";
+import { nanoid } from "nanoid"; // not sure if I need this, but may need a way to reference incorrect answers with an ID
 
 // API - https://opentdb.com/api_config.php
 // https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
@@ -24,30 +25,87 @@ function App() {
   const getQuiz = () => {
     Axios.get("https://opentdb.com/api.php?amount=5").then((response) => {
       const data = response.data.results;
-      console.log(response.data.results[0].incorrect_answers);
       setQuiz(data);
     });
   };
 
-  const answers = quiz.map(
-    ({
-      question: question,
-      correct_answer: correct_answer,
-      incorrect_answers: incorrect_answers,
-    }) => (
-      <div>
-        {console.log(typeof incorrect_answers)}
-        <h1>{sanitizeHtml(question)}</h1>
-        <Answer value={sanitizeHtml(correct_answer)} />
-        <Answer value={sanitizeHtml(incorrect_answers)} />
-      </div>
-    )
-  );
+  // answers array
 
-  // let answers = quiz.incorrect_answers;
-  // console.log(answers);
-  // answers.push(quiz.correct_answer);
-  // console.log(answers);
+  const [chosenAnswers, setChosenAnswers] = useState([]);
+
+  var shuffle = require("shuffle-array");
+
+  // multiple entries, so we map through and grab the correct_answer + incorrect_answers, add both to a new array and then shuffle, then render that resulting array
+  const answers = quiz.map(function ({
+    question: question,
+    correct_answer: correct_answer,
+    incorrect_answers: incorrect_answers,
+  }) {
+    const allAnswers = [];
+    incorrect_answers.map((answer) =>
+      allAnswers.push(incorrectAnswerObject(answer))
+    );
+    allAnswers.push(correctAnswerObject(correct_answer));
+    shuffle(allAnswers);
+
+    return (
+      <div>
+        <h1>{sanitizeHtml(question)}</h1>
+        {allAnswers.map((answer) => (
+          <div>
+            <Answer
+              key={answer.id}
+              correct_answer={answer.correct_answer}
+              value={answer.value}
+              select_answer={answer.selected_answer}
+              clickAnswer={() => clickAnswer(answer.id)}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  });
+
+  function correctAnswerObject(value) {
+    return {
+      value: sanitizeHtml(value),
+      correct_answer: true,
+      id: nanoid(),
+      selected_answer: false,
+    };
+  }
+
+  function incorrectAnswerObject(value) {
+    return {
+      value: value,
+      correct_answer: false,
+      id: nanoid(),
+      selected_answer: false,
+    };
+  }
+
+  // function clickAnswer(id) {
+  //   setChosenAnswers((previousAnswer) =>
+  //     previousAnswer.map((answer) => {
+  //       return id === answer.id
+  //         ? { ...answer, selected_answer: !answer.selected_answer }
+  //         : answer;
+  //     })
+  //   );
+  // }
+
+  console.log(chosenAnswers);
+
+  function clickAnswer(id) {
+    setChosenAnswers();
+    setChosenAnswers((previousAnswer) =>
+      previousAnswer.map((answer) => {
+        return id === answer.id
+          ? { ...answer, selected_answer: !answer.selected_answer }
+          : answer;
+      })
+    );
+  }
 
   return (
     <div className="App">
--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function Answer(props) {
+  const styles = {
+    // backgroundColor: props.correct_answer && "#D6DBF5",
+    backgroundColor: props.selected_answer && "#D6DBF5",
+  };
+
   return (
     <div>
-      <button>{props.value}</button>
+      <button style={styles} onClick={props.clickAnswer}>
+        {props.value}
+      </button>
     </div>
   );
 }
