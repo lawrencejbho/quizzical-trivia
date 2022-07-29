328475ef76d72eab48536d9a866bcea75bb1f6c7
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-07-29 11:58:50 -0700

    quiz now does api call immediately, merged the functionality of the get quiz and start quiz button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import Axios from "axios";
 import sanitizeHtml from "sanitize-html"; // need this to help with the format of the API
 import Starter from "./components/Starter.js";
 import Answer from "./components/Answer.js";
+import Check from "./components/Check.js";
 import { nanoid } from "nanoid";
 
 // API - https://opentdb.com/api_config.php
 // https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
 
 function App() {
+  // this is going to help us immediately fetch the quiz so there's no delay when we hit the start Quiz
+  useEffect(() => getQuiz(), []);
+
   // for the starter screen
   const [start, setStart] = useState(false);
+
+  const getQuiz = () => {
+    Axios.get("https://opentdb.com/api.php?amount=2").then((response) => {
+      const data = response.data.results;
+      setQuiz(processData(data));
+    });
+  };
 
   function isStart() {
     setStart(true);
@@ -22,16 +33,9 @@ function App() {
 
   const [quiz, setQuiz] = useState([]);
 
-  const getQuiz = () => {
-    Axios.get("https://opentdb.com/api.php?amount=2").then((response) => {
-      const data = response.data.results;
-      setQuiz(processData(data));
-    });
-  };
-
-  // process the Data to make it a lot easier to work with later on and can assign additional properties
-
+  // process the Data to make it a lot easier to work as answers will be objects
   function processData(data) {
+    // helper function to add additional properties and shuffle our answers
     function formatAnswers(correctAnswer, incorrectAnswers) {
       return [
         {
@@ -51,6 +55,8 @@ function App() {
       ].sort(() => (Math.random() > 0.5 ? 1 : -1));
     }
 
+    // after formatting each answer, we'll drop them into a combined answers array.
+    // each question itself will also have it's own id
     return data.map((item) => {
       return {
         id: nanoid(),
@@ -63,39 +69,38 @@ function App() {
     });
   }
 
-  // when I click on an answer, I'm getting an uncaught type error
-  const quizElements = quiz.map((item) => {
-    console.log(quiz);
-    if (item == undefined) return;
-    return (
-      <div>
+  const quizElements =
+    start &&
+    quiz.map((item) => {
+      return (
         <div>
-          <h1>{sanitizeHtml(item.question)}</h1>
-          {item.answers.map((answer) => (
-            <Answer
-              key={answer.id}
-              correct={answer.correct}
-              value={answer.answer}
-              selected={answer.selected}
-              clickAnswer={() => clickAnswer(item.id, answer.id)}
-            />
-          ))}
+          <div>
+            <h1>{sanitizeHtml(item.question)}</h1>
+            {item.answers.map((answer) => (
+              <Answer
+                key={answer.id}
+                correct={answer.correct}
+                value={answer.answer}
+                selected={answer.selected}
+                clickAnswer={() => clickAnswer(item.id, answer.id)}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
 
-  // we need to check for questionId here so that we can select an answer on a per question basis
+  // need to check for questionId here so that we can select an answer on a per question basis
   function clickAnswer(questionId, answerId) {
     setQuiz((prevValues) => {
       return prevValues.map((item) => {
         if (item.id == questionId) {
           return {
-            ...item, // we need to map inside the return object or we will lose {...item} which will change our original array as we return upwards
+            ...item, // need to map inside the return object or it will lose {...item} which will change our original array as we return upwards
             answers: item.answers.map((answer) => {
               return answerId === answer.id
                 ? { ...answer, selected: !answer.selected }
-                : { ...answer, selected: false }; // if we wanted to multi select, we leave this as answer, this instead makes it so that only one answer can be selected at a time
+                : { ...answer, selected: false }; // if I wanted to multi select, then leave this as answer, this instead makes it so that only one answer can be selected at a time
             }),
           };
         }
@@ -104,11 +109,17 @@ function App() {
     });
   }
 
+  function checkAnswers() {
+    return;
+  }
+
+  const checkElements = quiz == [] && <Check />;
+
   return (
     <div className="App">
       {starterElements}
       {quizElements}
-      <button onClick={getQuiz}>Get Quiz</button>
+      {checkElements}
     </div>
   );
 }
---
+++ src/components/Check.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Check() {
+  return (
+    <>
+      <button>Check Answers</button>
+    </>
+  );
+}
+
+export default Check;
