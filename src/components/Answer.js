83748d9585b6831e73e7a5a36110d5849f27a961
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-08-03 10:44:11 -0700

    added in incorrect answer styling

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -3,12 +3,15 @@ import React from "react";
 function Answer(props) {
   function changeColors() {
     if (props.correct && props.checkAnswers) {
-      return "#94D7A2";
+      return "#94D7A2"; // change to green
+    } else if (props.selected && props.checkAnswers) {
+      return "#F8BCBC"; // change to red
     } else if (props.selected) {
-      return "#D6DBF5";
+      return "#D6DBF5"; // change to blue
     }
   }
 
+  // remove the button border for selected answers and correct answers
   function changeBorder() {
     if (props.selected || (props.correct && props.checkAnswers)) {
       return "none";
